c2697aa9c67fff7da562ff4f4f3232450223ee5d
Maria Paula Coser Zamin <mariapaulacoserzamin@gmail.com>
2023-03-14 21:10:15 -0300

    Renaming files

--- agent_ppo_v2_evaluate.py
+++ agent_ppo_v2_evaluate.py
@@ -275,7 +275,7 @@ info = ray.init(ignore_reinit_error=True)
 config = ppo.PPOConfig()
 config.environment(env=ShowerEnv)
 agent = config.build()
-checkpoint_root = "C:\\Users\\maria\\ray_ppo_checkpoints\\agent_ppo_v3\\checkpoint_000050"
+checkpoint_root = "C:\\Users\\maria\\ray_ppo_checkpoints\\agent_ppo_v2\\checkpoint_000050"
 agent.restore(checkpoint_root)
 
 # Constrói o ambiente:
