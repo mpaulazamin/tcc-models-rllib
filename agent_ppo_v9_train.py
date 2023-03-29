ce9222f1a640199738fd032aa829ecede78d83bd
Maria Paula Coser Zamin <mariapaulacoserzamin@gmail.com>
2023-03-29 14:08:10 -0300

    Adding files

--- agent_ppo_v9_train.py
+++ agent_ppo_v9_train.py
@@ -238,7 +238,7 @@ class ShowerEnv(gym.Env):
 
 
 # Folder para checkpoints:
-checkpoint_root = "C:\\Users\\maria\\ray_ppo_checkpoints\\agent_ppo_v9"
+checkpoint_root = "C:\\Users\\maria\\ray_ppo_checkpoints\\agent_ppo_v9_Tinf25"
 shutil.rmtree(checkpoint_root, ignore_errors=True, onerror=None)
 
 # Folder para os resultados:
@@ -261,7 +261,7 @@ results = []
 episode_data = []
 
 # Realiza o treinamento:
-n_iter = 51
+n_iter = 101
 for n in range(1, n_iter):
 
     # Treina o agente:
@@ -290,7 +290,7 @@ for n in range(1, n_iter):
 # Salva resultados e plota dados do episódio:
 print(results)
 df = pd.DataFrame(data=episode_data)
-df.to_csv("episode_data_agent_ppo_v9.csv")
+df.to_csv("episode_data_agent_ppo_v9_Tinf25.csv")
 
 policy = agent.get_policy()
 model = policy.model
