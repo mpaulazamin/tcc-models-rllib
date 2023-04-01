2a02d482ba0322a717469b0cd895be6a30a5c0e2
Maria Paula Coser Zamin <mariapaulacoserzamin@gmail.com>
2023-04-01 10:50:54 -0300

    Adding results

--- agent_ppo_v10_train.py
+++ agent_ppo_v10_train.py
@@ -39,9 +39,9 @@ class ShowerEnv(gym.Env):
 
         # Distúrbios e temperatura ambiente - Fd, Td, Tf, Tinf:
         self.Fd = 0
-        self.Td = 25
-        self.Tf = 25
-        self.Tinf = 25
+        self.Td = 20
+        self.Tf = 20
+        self.Tinf = 20
 
         # Não utiliza split-range:
         self.split_range = 0
@@ -236,7 +236,7 @@ class ShowerEnv(gym.Env):
 
 
 # Folder para checkpoints:
-checkpoint_root = "C:\\Users\\maria\\ray_ppo_checkpoints\\agent_ppo_v10_Tinf25"
+checkpoint_root = "C:\\Users\\maria\\ray_ppo_checkpoints\\agent_ppo_v10_Tinf20"
 shutil.rmtree(checkpoint_root, ignore_errors=True, onerror=None)
 
 # Folder para os resultados:
@@ -288,7 +288,7 @@ for n in range(1, n_iter):
 # Salva resultados e plota dados do episódio:
 print(results)
 df = pd.DataFrame(data=episode_data)
-df.to_csv("episode_data_agent_ppo_v10_Tinf25.csv")
+df.to_csv("episode_data_agent_ppo_v10_Tinf20.csv")
 
 policy = agent.get_policy()
 model = policy.model
